66a6e2432abdbc42ac7dff0c333dabdf867f5050
Eli Wemyss <eliwemyss@gmail.com>
2018-12-03 22:26:07 -0800

    added schema

---
+++ server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require('./config');
+
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(express.json());
+
+
+let server;
+
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl,{ useNewUrlParser: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if(err) {
+                    return reject(err)
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };

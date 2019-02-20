f5a562dd35d99002f4ee4db273493a7644371a73
Eli Wemyss <eliwemyss@gmail.com>
2019-02-19 16:36:15 -0800

    trying to pass Heroku connection issue

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/declutterly-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/declutterly-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-declutterly-app';
 exports.API_BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3000/api'
 exports.PORT = process.env.PORT || 8080;
